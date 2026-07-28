565b4f88e611f57c5e205839a76115a9c79c8c4f
Alex Terev <2011shtab2012@gmail.com>
2026-07-29 02:25:27 +0300

    wip(order): implement create order endpoint

--- order-service/src/main/java/com/nd/orderservice/order/api/OrderController.java
+++ order-service/src/main/java/com/nd/orderservice/order/api/OrderController.java
@@ -2,6 +2,7 @@ package com.nd.orderservice.order.api;
 
 import com.nd.orderservice.order.application.OrderService;
 import com.nd.orderservice.order.api.mapper.CreateOrderMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,10 @@ public class OrderController {
     private final CreateOrderMapper mapper;
 
     @PostMapping
-    public CreateOrderResponse create(@RequestBody CreateOrderRequest createOrderRequest) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public CreateOrderResponse create(@Valid @RequestBody CreateOrderRequest request) {
+        var command = mapper.toCommand(request);
+        var order = orderService.create(command);
+
+        return mapper.toResponse(order);
     }
 }
