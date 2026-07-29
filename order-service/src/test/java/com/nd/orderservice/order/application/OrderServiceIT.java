61aa299ac7796a35da77a36d3d43fc0775e665b2
Alex Terev <2011shtab2012@gmail.com>
2026-07-29 21:35:25 +0300

    test(order): verify order and outbox persistence

---
+++ order-service/src/test/java/com/nd/orderservice/order/application/OrderServiceIT.java
@@ -0,0 +1,63 @@
+package com.nd.orderservice.order.application;
+
+
+import com.nd.orderservice.order.application.command.CreateOrderCommand;
+import com.nd.orderservice.order.infrastructure.outbox.OutboxEventRepository;
+import com.nd.orderservice.order.persistence.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * @since 2026
+ */
+@SpringBootTest
+class OrderServiceIT {
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OutboxEventRepository outboxEventRepository;
+
+    @Test
+    void create() {
+        var customerId = UUID.randomUUID();
+        var productId = UUID.randomUUID();
+        var items = List.of(new CreateOrderCommand.Item(productId, "Item 1", BigDecimal.valueOf(100), 10));
+
+        var command = new CreateOrderCommand(customerId, items);
+
+        var order = orderService.create(command);
+
+        assertTrue(orderRepository.existsById(order.getId()));
+        assertNotNull(order.getItems());
+        assertFalse(order.getItems().isEmpty());
+
+        var events = outboxEventRepository.findAll();
+
+        assertNotNull(events);
+        assertFalse(events.isEmpty());
+
+        var eventOpt = events.stream()
+                .filter(event -> event.getAggregateId().equals(order.getId()))
+                .findAny();
+
+        assertTrue(eventOpt.isPresent());
+
+        var event = eventOpt.get();
+
+        assertEquals("ORDER_CREATED", event.getType());
+    }
+}
