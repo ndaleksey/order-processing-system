38949887d06bfd5c711225c2619c6cde87620c04
Alex Terev <2011shtab2012@gmail.com>
2026-07-30 13:53:54 +0300

    wip: add Kafka producer infrastructure

--- order-service/src/main/java/com/nd/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/nd/orderservice/OrderServiceApplication.java
@@ -1,8 +1,12 @@
 package com.nd.orderservice;
 
+import com.nd.orderservice.order.infrastructure.kafka.KafkaTopicsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class OrderServiceApplication {
 
---
+++ order-service/src/main/java/com/nd/orderservice/order/infrastructure/kafka/KafkaTopicsProperties.java
@@ -0,0 +1,10 @@
+package com.nd.orderservice.order.infrastructure.kafka;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @since 2026
+ */
+@ConfigurationProperties(prefix="app.kafka.topics")
+public record KafkaTopicsProperties(String orderEvents) {
+}
